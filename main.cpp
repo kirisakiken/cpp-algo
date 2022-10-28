1f42b1717d003e40adb1d64e1b9ea134c703cbdb
kirisakiken <bezmican96@gmail.com>
2022-10-28 18:29:52 +0300

    feat: update main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
 
 #include "problems/leetcode.h"
 #include "problems/leetcode75.h"
 #include "data_structures/list_node.h"
-#include "problems/tester.cpp"
 
 template<typename T>
 void printVector(const std::vector<T>& vec) {
   std::cout << "[ ";
   for (int i = 0; i < vec.size(); ++i) {
     std::cout << vec[i] << ", ";
+  }
+  std::cout << "]" << std::endl;
+}
+
+template<typename T>
+void printJaggedVector(const std::vector<std::vector<T>>& vec) {
+  std::cout << "[ ";
+  for (const auto& x : vec) {
+    std::cout << "[ ";
+    for (const auto& y : x) {
+      std::cout << y << ", ";
+    }
+    std::cout << " ]";
+  }
+  std::cout << " ]" << std::endl;
+}
+
+void printTreeNodeVector(const std::vector<TreeNode*>& vec) {
+  std::cout << "[ ";
+  for (auto it = vec.begin(); it != vec.end(); ++it) {
+    std::cout << (*it)->val << ", ";
   }
   std::cout << "]" << std::endl;
 }
@@ -26,9 +50,8 @@ void printLinkedList(leetcode::ListNode* head) {
 }
 
 int main() {
-  std::vector<int> vec{1, 2, 3};
-//  auto res = leetcode::plusOne(vec);
-//  printVector(res);
-  std::cout << *(vec.end() - 1);
+  int res = lengthOfLastWord("a ");
+  std::cout << res << std::endl;
+
   return 0;
 }

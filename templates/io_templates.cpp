2aa6823d630454f38f3c649a6329b6595b570a49
kirisakiken <bezmican96@gmail.com>
2022-12-11 00:28:31 +0300

    feat: implement io templates

--- templates/io_templates.cpp
+++ templates/io_templates.cpp
@@ -1,5 +1,68 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 #include "io_templates.h"
 
+template<typename T>
+void printVector(const std::vector<T>& vec) {
+  std::cout << "[ ";
+  for (int i = 0; i < vec.size(); ++i) {
+    std::cout << vec[i] << ", ";
+  }
+  std::cout << "]" << std::endl;
+}
 
+template<typename T>
+void printJaggedVector(const std::vector<std::vector<T>>& vec) {
+  std::cout << "[ ";
+  for (const auto& x : vec) {
+    std::cout << "[ ";
+    for (const auto& y : x) {
+      std::cout << y << ", ";
+    }
+    std::cout << " ]\n";
+  }
+  std::cout << " ]" << std::endl;
+}
+
+void printTreeNodeVector(const std::vector<leetcode::TreeNode*>& vec) {
+  std::cout << "[ ";
+  for (auto it = vec.begin(); it != vec.end(); ++it) {
+    std::cout << (*it)->val << ", ";
+  }
+  std::cout << "]" << std::endl;
+}
+
+void printLinkedListVector(const std::vector<leetcode::ListNode*>& vec) {
+  std::cout << "[ ";
+  for (const auto& e : vec) {
+    std::cout << e->val << ", ";
+  }
+  std::cout << "]" << std::endl;
+}
+
+void printLinkedList(leetcode::ListNode* head) {
+  std::string res = "[";
+  while (head != nullptr) {
+    res += std::to_string(head->val);
+    res += ",";
+    head = head->next;
+  }
+  res += "]";
+  std::cout << res << std::endl;
+}
+
+std::vector<int> vectorSlice(std::vector<int>& vec, const int& start, const int& end) {
+  std::vector<int> res(end - start + 1);
+  std::copy(vec.begin() + start, vec.begin() + end + 1, res.begin());
+  return res;
+}
+
+std::vector<int> excludeIndex(const std::vector<int>& vec, const int& index) {
+  std::vector<int> res(vec.size() - 1);
+  std::copy(vec.begin(), vec.begin() + index, res.begin());
+  std::copy(vec.begin() + index + 1, vec.end(), res.begin() + index);
+
+  return res;
+}
--- templates/io_templates.h
+++ templates/io_templates.h
@@ -1,3 +1,27 @@
 #pragma once
 
+#include "../data_structures/tree_node.h"
+#include "../data_structures/list_node.h"
 
+/// Prints given vector
+template<typename T>
+void printVector(const std::vector<T>& vec);
+
+/// Prints elements of jagged vector. aka matrix
+template<typename T>
+void printJaggedVector(const std::vector<std::vector<T>>& vec);
+
+/// Prints given vector of tree nodes
+void printTreeNodeVector(const std::vector<leetcode::TreeNode*>& vec);
+
+/// Prints given vector of linked list
+void printLinkedListVector(const std::vector<leetcode::ListNode*>& vec);
+
+/// Prints given linked list
+void printLinkedList(leetcode::ListNode* head);
+
+/// Slices a given vector using start/end indices and returns new vector
+std::vector<int> vectorSlice(std::vector<int>& vec, const int& start, const int& end);
+
+/// Given vector, excludes the given index and returns new vector
+std::vector<int> excludeIndex(const std::vector<int>& vec, const int& index);

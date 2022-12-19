58e9ab2bfa60f83a8a32933f4c959d93a329eaa0
kirisakiken <bezmican96@gmail.com>
2022-12-20 02:35:44 +0300

    feat: update main

--- main.cpp
+++ main.cpp
@@ -3,18 +3,22 @@
 #include <algorithm>
 #include <unordered_map>
 #include <map>
+#include <bitset>
+#include <queue>
+#include <stack>
 
 #include "algo_tech/combination.h"
 #include "algo_tech/permutation.h"
 #include "templates/io_templates.h"
 #include "problems/leetcode.h"
+#include "templates/algo_templates.h"
 
 using namespace leetcode;
 
 
 int main() {
   std::vector<int> values = {1, 2, 3};
-  auto res = permutations(values);
+  auto res = get_permutations(values);
   printJaggedVector(res);
 
   return 0;
